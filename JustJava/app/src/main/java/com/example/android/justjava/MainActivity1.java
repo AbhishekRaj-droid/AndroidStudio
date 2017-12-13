ee79afef8bd0d8615ad8c11b83d8e2b238a27d76
deepclearning <deepclearning@gmail.com>
2017-12-13 20:35:22 +0530

    Adding something in just java, making user interactive

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity1.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity1.java
@@ -15,11 +15,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 /**
  * This app displays an order form to order coffee.
  */
 public class MainActivity1 extends AppCompatActivity {
 
+
+    int numberofcoffees=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,14 +34,28 @@ public class MainActivity1 extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(1);
+        display(numberofcoffees);
+        displayprice(numberofcoffees * 5);
     }
 
     /**
      * This method displays the given quantity value on the screen.
      */
+    public void increment(View view) {
+        numberofcoffees += 1;
+        display(numberofcoffees);
+    }
+
+    public void decrement(View view) {
+        numberofcoffees -= 1;
+        display(numberofcoffees);
+    }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+    private void displayprice(int number) {
+        TextView priceTextView = (TextView)findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
