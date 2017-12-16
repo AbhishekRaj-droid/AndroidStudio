2b9c91f010a2ba7c26aed39979f50bbcef29d3e3
deepclearning <deepclearning@gmail.com>
2017-12-17 01:03:48 +0530

    Adding chocolate and cream in tea, wow its awesome

--- JustJava/app/src/main/java/com/example/android/justjava/MainActivity1.java
+++ JustJava/app/src/main/java/com/example/android/justjava/MainActivity1.java
@@ -13,6 +13,7 @@ package com.example.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -27,6 +28,7 @@ public class MainActivity1 extends AppCompatActivity {
     int cnt = 1;
     String first = "Abhishek";
     String last = "Raj";
+    int price;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,11 +41,24 @@ public class MainActivity1 extends AppCompatActivity {
     public void submitOrder(View view) {
         String name = displayname(first, last);
         displayquantity(numberofcoffees);
-        int price = calculateprice(numberofcoffees, 5);
+        boolean hascream = haswhippedcream();
+        boolean chocolate = havechocolate();
+        if (hascream && chocolate) {
+            price = calculateprice(numberofcoffees, 5 + 2);
+        }
+        else if (hascream || chocolate) {
+            price = calculateprice(numberofcoffees, 5 + 1);
+        }
+        else {
+            price = calculateprice(numberofcoffees, 5);
+        }
         String pricemessage = "\nTotal: $" + price;
         String quantitymessage = "\nQuantity: " + numberofcoffees;
         String greetingmessage = "\nThank you!";
-        displayMessage(name + quantitymessage + pricemessage + greetingmessage);
+        String whippedcream = "\nAdded Whipped cream ? ";
+        String chocomessage = "\nAdd Chocolate? ";
+        displayMessage(name,  quantitymessage , pricemessage , greetingmessage, whippedcream, hascream, chocomessage, chocolate);
+
     }
 
     /**
@@ -66,7 +81,8 @@ public class MainActivity1 extends AppCompatActivity {
         TextView priceTextView = (TextView)findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
-    private void displayMessage(String message) {
+    private void displayMessage(String name, String quantity, String price, String greet, String cream, boolean hascream, String addchoco, boolean choco) {
+        String message = name + cream + hascream + addchoco + choco + quantity + price + greet;
         TextView orderSummaryTextview = (TextView)findViewById(R.id.order_summary_text_view);
         orderSummaryTextview.setText(message);
     }
@@ -77,5 +93,15 @@ public class MainActivity1 extends AppCompatActivity {
 
     private String displayname(String first, String last) {
         return "Name: " + first + " " + last;
+    }
+
+    private boolean haswhippedcream() {
+        CheckBox hasTakenwhipped = (CheckBox)findViewById(R.id.checkbox_whipped_cream);
+        return hasTakenwhipped.isChecked();
+    }
+
+    private boolean havechocolate() {
+        CheckBox havechoc = (CheckBox)findViewById(R.id.checkbox_chocolate);
+        return havechoc.isChecked();
     }
 }
